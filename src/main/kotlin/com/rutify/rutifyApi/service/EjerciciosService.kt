0df7948403c130d150e1914cabd000db381b5431
dganicolas <dganicolas@rafaelalberti.es>
2025-05-22 20:40:36 +0200

    modulo de rutinas finalizado y refactorizacion de codigo

--- src/main/kotlin/com/rutify/rutifyApi/service/EjerciciosService.kt
+++ src/main/kotlin/com/rutify/rutifyApi/service/EjerciciosService.kt
@@ -21,10 +21,7 @@ class EjerciciosService {
     private lateinit var mongoTemplate: MongoTemplate
 
     fun crearEjercicio(ejercicioDTO: EjercicioDTO): ResponseEntity<Ejercicio> {
-        val error = validarEjercicio(ejercicioDTO)
-        if (error != null) {
-            throw ValidationException(error)
-        }
+        validarEjercicio(ejercicioDTO)
 
         val ejercicio = Ejercicio(
             id = null,
@@ -41,62 +38,61 @@ class EjerciciosService {
         return ResponseEntity.ok(ejercicioGuardado)
     }
 
-    private fun validarEjercicio(ejercicio: EjercicioDTO): String? {
-        if (ejercicio.nombreEjercicio.isBlank()) return "El nombre del ejercicio no puede estar vacío"
-        if (ejercicio.descripcion.isBlank()) return "La descripción no puede estar vacía"
-        if (ejercicio.imagen.isBlank()) return "La URL de la imagen no puede estar vacía"
-        if (!ejercicio.imagen.startsWith("http")) return "La URL de la imagen no es válida"
-        if (ejercicio.grupoMuscular.isBlank()) return "El grupo muscular no puede estar vacío"
-        if (ejercicio.caloriasQuemadasPorRepeticion <= 0) return "Las calorías por repetición deben ser mayores que 0"
-        if (ejercicio.puntoGanadosPorRepeticion <= 0) return "Los puntos por repetición deben ser mayores que 0"
-        return null
+    private fun validarEjercicio(ejercicio: EjercicioDTO) {
+        if (ejercicio.nombreEjercicio.isBlank()) throw ValidationException("El nombre del ejercicio no puede estar vacío")
+        if (ejercicio.descripcion.isBlank()) throw ValidationException("La descripción no puede estar vacía")
+        if (ejercicio.imagen.isBlank()) throw ValidationException("La URL de la imagen no puede estar vacía")
+        if (!ejercicio.imagen.startsWith("http")) throw ValidationException("La URL de la imagen no es válida")
+        if (ejercicio.grupoMuscular.isBlank()) throw ValidationException("El grupo muscular no puede estar vacío")
+        if (ejercicio.caloriasQuemadasPorRepeticion <= 0) throw ValidationException("Las calorías por repetición deben ser mayores que 0")
+        if (ejercicio.puntoGanadosPorRepeticion <= 0) throw ValidationException("Los puntos por repetición deben ser mayores que 0")
     }
 
     fun obtenerEjercicios(
-            grupoMuscular: String?,
-            equipo: String?,
-            page: Int?,
-            size: Int?
-        ): List<EjercicioDTO> {
-            // Si no hay filtros, devolver todos los ejercicios
-            val filtroGrupoMuscular = grupoMuscular?.takeIf { it.isNotBlank() }
-            val filtroEquipo = equipo?.takeIf { it.isNotBlank() }
+        grupoMuscular: String?,
+        equipo: String?,
+        page: Int?,
+        size: Int?,
+    ): List<EjercicioDTO> {
+        // Si no hay filtros, devolver todos los ejercicios
+        val filtroGrupoMuscular = grupoMuscular?.takeIf { it.isNotBlank() }
+        val filtroEquipo = equipo?.takeIf { it.isNotBlank() }
 
-            val query = Query()
+        val query = Query()
 
-            // Aplicar filtros si se pasan
-            filtroGrupoMuscular?.let {
-                query.addCriteria(Criteria.where("grupoMuscular").regex(it, "i"))
-            }
-
-            filtroEquipo?.let {
-                query.addCriteria(Criteria.where("equipo").regex(it, "i"))
-            }
-
-            // Aplicar paginación si se pasan los parámetros
-            page?.let {
-                query.skip((it * (size ?: 10)).toLong())  // Calcular el offset (skip)
-            }
-
-            size?.let {
-                query.limit(it)  // Limitar el tamaño de la página
-            }
-            val ejercicios = mongoTemplate.find(query, Ejercicio::class.java)
-
-            // Consultar los ejercicios con la query preparada
-            return ejercicios.map { ejercicio ->
-                // Aquí conviertes de entidad `Ejercicio` a DTO si lo necesitas
-                EjercicioDTO(
-                    id = ejercicio.id!!,
-                    nombreEjercicio = ejercicio.nombreEjercicio,
-                    descripcion = ejercicio.descripcion,
-                    equipo = ejercicio.equipo,
-                    grupoMuscular = ejercicio.grupoMuscular,
-                    imagen = ejercicio.imagen,
-                    caloriasQuemadasPorRepeticion = ejercicio.caloriasQuemadasPorRepeticion,
-                    puntoGanadosPorRepeticion = ejercicio.puntoGanadosPorRepeticion,
-                    cantidad = 0
-                )
-            }
+        // Aplicar filtros si se pasan
+        filtroGrupoMuscular?.let {
+            query.addCriteria(Criteria.where("grupoMuscular").regex(it, "i"))
         }
+
+        filtroEquipo?.let {
+            query.addCriteria(Criteria.where("equipo").regex(it, "i"))
+        }
+
+        // Aplicar paginación si se pasan los parámetros
+        page?.let {
+            query.skip((it * (size ?: 10)).toLong())  // Calcular el offset (skip)
+        }
+
+        size?.let {
+            query.limit(it)  // Limitar el tamaño de la página
+        }
+        val ejercicios = mongoTemplate.find(query, Ejercicio::class.java)
+
+        // Consultar los ejercicios con la query preparada
+        return ejercicios.map { ejercicio ->
+            // Aquí conviertes de entidad `Ejercicio` a DTO si lo necesitas
+            EjercicioDTO(
+                id = ejercicio.id!!,
+                nombreEjercicio = ejercicio.nombreEjercicio,
+                descripcion = ejercicio.descripcion,
+                equipo = ejercicio.equipo,
+                grupoMuscular = ejercicio.grupoMuscular,
+                imagen = ejercicio.imagen,
+                caloriasQuemadasPorRepeticion = ejercicio.caloriasQuemadasPorRepeticion,
+                puntoGanadosPorRepeticion = ejercicio.puntoGanadosPorRepeticion,
+                cantidad = 0
+            )
+        }
+    }
 }
